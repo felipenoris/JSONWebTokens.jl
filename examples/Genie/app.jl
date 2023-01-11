5c760ffeba98bcf37ef97ff17adf83f6ae027429
Abhi <8083613+AbhimanyuAryan@users.noreply.github.com>
2023-01-11 10:31:57 +0530

    finished example

--- examples/Genie/app.jl
+++ examples/Genie/app.jl
@@ -4,34 +4,47 @@ using Genie.Renderer.Json
 using Genie.Requests
 using Genie.Responses
 using Genie.Cookies
+using Genie.Encryption
+
+const jwt_secret_key = ENV["JWT_SECRET_KEY"]
+const encoding = JSONWebTokens.HS256(jwt_secret_key)
 
 function generateToken(user)
-    jwt_secret_key = ENV["JWT_SECRET_KEY"]
-    encoding = JSONWebTokens.HS256(jwt_secret_key)
     jwttoken = JSONWebTokens.encode(encoding, user)
     return jwttoken
-    #Genie.Cookies.set!(Genie.Response.getresponse(), {"token" => jwttoken});
 end
 
-route("/api/create_new_user", method = POST) do
+function cookie_jwt_auth(req, res)
+    cookie = req["Cookie"][7:end]
+    # mycookie = Genie.Cookies.getcookies(req)
+    token = Genie.Encryption.decrypt(String(cookie))
+    try
+        user = JSONWebTokens.decode(encoding, token)
+        @info user
+    catch ex
+        # if signature doesn't verify
+        # JSONWebTokens.InvalidSignatureError()
+        println(ex)
+        # TODO: implement clear_cookie
+        # very very very important -> res.clear_cookie
+        # very very very important -> redirect to root
+    end 
+end
+
+route("/login_route", method = POST) do
     user = Dict( "sub" => "1234567890", "name" => "John Doe", "iat" => 1516239022)
     token = generateToken(user)
+    @info token
     Genie.Cookies.set!(Genie.Responses.getresponse(), :token, token, Dict(
         "httponly" => true,
         "maxage" => 3600
     ))
 end
 
-route("/user/validateToken",  method = GET) do
+route("/add_route",  method = POST) do
     req = Genie.Requests.request()
-    @info req
-    @info req.headers
-    @info req.headers
-    # @info Genie.Requests.request.headers
-    # @info Genie.Requets.request.headers["Authorization"]
-    # headers = Dict(req.headers)
-    # auth_header = headers["Authorization"]
-    # @info auth_header
-    # token = split(auth_header, " ")[1]
-    # @info token
+    res = Genie.Responses.getresponse()
+    cookie_jwt_auth(req, res)
+
+    @info "welcome to genie!"
 end
